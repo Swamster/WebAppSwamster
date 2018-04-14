8e948592ebca93015ac3e80ad1f342fb9fb0f3fc
Aria <akydd@wpi.edu>
2018-04-13 23:41:31 -0400

    First webapp commit

---
+++ swamstergradle/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.io.FileInputStream;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		FileInputStream serviceAccount = new FileInputStream("C:\Users\Aria\eclipse\java-oxygen\eclipse\jsonswamster");
+//
+//		FirebaseOptions options = new FirebaseOptions.Builder()
+//		    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//		    .setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com/")
+//		    .build();
+//
+//		FirebaseApp.initializeApp(options);
+
+	
+	
+	
+//	
+//		FirebaseApp.initializeApp();
+	
+	}
+
+}
