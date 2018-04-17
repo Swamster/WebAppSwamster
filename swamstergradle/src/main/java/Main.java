8f637894e2380613312f2134b5de4a34980b066b
Aria <akydd@wpi.edu>
2018-04-17 16:23:21 -0400

    All classes working

--- swamstergradle/src/main/java/Main.java
+++ swamstergradle/src/main/java/Main.java
@@ -1,9 +1,19 @@
 import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Main {
 
-	public static void main(String[] args) {
-//		FileInputStream serviceAccount = new FileInputStream("C:\Users\Aria\eclipse\java-oxygen\eclipse\jsonswamster");
+	public static void main(String[] args) throws IOException {
+//		FileInputStream serviceAccount = new FileInputStream("C:\\Users\\Aria\\eclipse\\java-oxygen\\eclipse\\jsonswamster");
 //
 //		FirebaseOptions options = new FirebaseOptions.Builder()
 //		    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -11,12 +21,37 @@ public class Main {
 //		    .build();
 //
 //		FirebaseApp.initializeApp(options);
+		
+		// Fetch the service account key JSON file contents
+		FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccount.json");
+
+		// Initialize the app with a service account, granting admin privileges
+		FirebaseOptions options = new FirebaseOptions.Builder()
+		    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+		    .setDatabaseUrl("https://<databaseName>.firebaseio.com")
+		    .build();
+		FirebaseApp.initializeApp(options);
+
+		// As an admin, the app has access to read and write all data, regardless of Security Rules
+		DatabaseReference ref = FirebaseDatabase.getInstance()
+		    .getReference("restricted_access/secret_document");
+		ref.addListenerForSingleValueEvent(new ValueEventListener() {
+		  @Override
+		  public void onDataChange(DataSnapshot dataSnapshot) {
+		    Object document = dataSnapshot.getValue();
+		    System.out.println(document);
+		  }
+
+		  @Override
+		  public void onCancelled(DatabaseError error) {
+		  }
+		});
 
 	
 	
 	
-//	
-//		FirebaseApp.initializeApp();
+	
+		
 	
 	}
 
--- swamstergradle/src/main/java/Post.java
+++ swamstergradle/src/main/java/Post.java
@@ -1,50 +1,32 @@
-//public static class Post {
-//
-//  public String author;
-//  public String title;
-//
-//  public Post(String author, String title) {
-//    // ...
-//  }
-//
-//}
-//
-//// Get a reference to our posts
-//final FirebaseDatabase database = FirebaseDatabase.getInstance();
-//DatabaseReference ref = database.getReference("server/saving-data/fireblog/posts");
-//]
-//// Attach a listener to read the data at our posts reference
-//ref.addValueEventListener(new ValueEventListener() {
-//  @Override
-//  public void onDataChange(DataSnapshot dataSnapshot) {
-//    Post post = dataSnapshot.getValue(Post.class);
-//    System.out.println(post);
-//  }
-//
-//  @Override
-//  public void onCancelled(DatabaseError databaseError) {
-//    System.out.println("The read failed: " + databaseError.getCode());
-//  }
-//  
-//  ref.addChildEventListener(new ChildEventListener() {
-//	  @Override
-//	  public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
-//	    Post newPost = dataSnapshot.getValue(Post.class);
-//	    System.out.println("Author: " + newPost.author);
-//	    System.out.println("Title: " + newPost.title);
-//	    System.out.println("Previous Post ID: " + prevChildKey);
-//	  }
-//
-//	  @Override
-//	  public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {}
-//
-//	  @Override
-//	  public void onChildRemoved(DataSnapshot dataSnapshot) {}
-//
-//	  @Override
-//	  public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {}
-//
-//	  @Override
-//	  public void onCancelled(DatabaseError databaseError) {}
-//	}
-//})
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Post {
+
+	public String author;
+	public String title;
+
+	public Post(String author, String title) {
+
+		// Get a reference to our posts
+		final FirebaseDatabase database = FirebaseDatabase.getInstance();
+		DatabaseReference ref = database.getReference("server/saving-data/fireblog/posts");
+
+		// Attach a listener to read the data at our posts reference
+		ref.addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				Post post = dataSnapshot.getValue(Post.class);
+				System.out.println(post);
+			}
+
+			@Override
+			public void onCancelled(DatabaseError databaseError) {
+				System.out.println("The read failed: " + databaseError.getCode());
+			}
+		});
+	}
+}
